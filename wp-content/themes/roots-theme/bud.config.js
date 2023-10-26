ea20ab30391ab0d989f5c714441ecdde54eceb32
James <jay.bells12@gmail.com>
2023-10-25 20:01:08 -0700

    building assets

--- wp-content/themes/roots-theme/bud.config.js
+++ wp-content/themes/roots-theme/bud.config.js
@@ -33,7 +33,7 @@ export default async (app) => {
    * @see {@link https://bud.js.org/docs/bud.watch}
    */
   app
-    .setUrl('http://localhost:3000')
+    .setUrl('http://sagetestd.wpengine.com')
     .setProxyUrl('http://example.test')
     .watch(['resources/views', 'app']);
 
